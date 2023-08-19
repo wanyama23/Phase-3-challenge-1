9fd3afe0eb3ad734f8d66af4f4e9022ac1582f6c
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-19 20:39:07 +0300

    displaying consonants

--- lib/challenge_3.py
+++ lib/challenge_3.py
@@ -13,7 +13,7 @@
 def consonants(string):
     consonants = []
     for char in string:
-        if char.lower() in ['a', 'e', 'i', 'o', 'u']:
+        if char.lower() not in ['a', 'e', 'i', 'o', 'u']:
             consonants.append(char)
     return consonants
 
@@ -24,3 +24,17 @@ string3 = 'how are you'
 print(consonants(string1))
 print(consonants(string2))
 print(consonants(string3))
+
+# def solve(s): 
+#     """replace all Consonant value with a " "(space) """
+#     empty = []
+#     result = re.sub(r'[AEIOU]', ' ', s, flags=re.IGNORECASE)
+
+#     solution = result.split(" ")
+#     for i in range(len(solution)):
+#         alphabet = string.ascii_lowercase  # латинский алфавит
+#         word = solution[i]
+#         ind_list = [sum(alphabet.index(x) + 1 for x in word.lower())]
+#         empty.append(' '.join(map(str, ind_list)))
+
+#     print(max(map(int, empty)))
